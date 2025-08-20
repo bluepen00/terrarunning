8dcfddc27324992d21fabec0e199918f9c012795
Mac <mac@Macs-MacBook-Pro-2.local>
2025-08-20 13:36:29 +0000

    add user data

--- main.tf
+++ main.tf
@@ -2,9 +2,21 @@ provider "aws" {
   region = "us-east-2"
 }
 
+
 resource "aws_instance" "example" {
-  ami           = "ami-0d1b5a8c13042c939" # Ubuntu 24.04 LTS // us-east-2
+  ami           = "ami-0fb653ca2d3203ac1" # Ubuntu 24.04 LTS // us-east-2
   instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html 
+              nohup busybox httpd -f -p 8080 &
+
+              EOF
+
+  user_data_replace_on_change = true
+
+
   tags = {
     Name = "terraform-test-instance"
   }
