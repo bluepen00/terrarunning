329e9c9a3822995f486bb9f3e4639fd81eca4aa3
Mac <mac@Macs-MacBook-Pro-2.local>
2025-08-20 15:28:59 +0000

    add security group

--- main.tf
+++ main.tf
@@ -4,12 +4,13 @@ provider "aws" {
 
 
 resource "aws_instance" "example" {
-  ami           = "ami-0fb653ca2d3203ac1" # New Ubuntu Iyesnstance 
-  instance_type = "t2.micro"
+  ami                    = "ami-0fb653ca2d3203ac1" # New Ubuntu Iyesnstance 
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance-sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html 
+              echo "Hello, World Tackie did this" > index.html 
               nohup busybox httpd -f -p 8080 &
 
               EOF
@@ -19,5 +20,14 @@ resource "aws_instance" "example" {
 
   tags = {
     Name = "terraform-test-instance"
+  }
+}
+
+resource "aws_security_group" "instance-sg" {
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
